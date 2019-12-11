fe22ff8f2724751dcca5aa67a8bcf69c20896345
Roman Zhak <4476842+zhakroman@users.noreply.github.com>
2019-12-11 22:20:21 +0300

    Update fb.py

--- fb.py
+++ fb.py
@@ -1,11 +1,21 @@
+### 1
+
+# 
+# python 2.7
+# range
+# xrange
+#
+
+### 2 
+
 # The generator of the Fibonacci numbers
 # @param  {N|int}: number 
 # @return {Object|generator}
 
-def fibonacci(N):
-    if N < 0:
-        raise ValueError('N >= 0 but got %i' % N)
+def fibonacci_generator(n):
+    if n < 0:
+        raise ValueError('n >= 0 but got %i' % n)
     previous, current = 0, 1
-    for i in range(0, N):
+    for i in range(0, n):
         yield current
         current, previous = current + previous, current
